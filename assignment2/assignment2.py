ab95186b87f873baf4bd430a4cbec9ccde59aaaa
Bilal <bilalkhattak7854@gmail.com>
2025-08-15 18:25:11 +0500

    pdf added

---
+++ assignment2/assignment2.py
@@ -0,0 +1,15 @@
+from math import pi
+
+
+
+def get_area(r):
+    return pi*r*r
+
+
+def output_parameter(r):
+    print("The parameter of the circle with radius ",r ,"is",2*pi*r)
+
+
+
+
+output_parameter(1)
